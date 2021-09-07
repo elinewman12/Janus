18231ca5f48b12341bfa85425b73002e6e30e530
John Widdifield <jfwiddif@ncsu.edu>
2021-09-07 13:05:18 -0400

    Added the KeyUtil key change class

---
+++ src/KeyUtil.py
@@ -0,0 +1,47 @@
+
+NOTES = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+EQUIVALENCE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+# This method will shift all notes in the song up (positive numHalfSteps) or
+# down (negative numHalfSteps) the number of half steps specified.
+#(assuming there are no key changes)
+#
+# <jfwiddif>
+def changeSongKeyByHalfSteps(song, numHalfSteps):
+    for track in song.tracks:
+        for note in track.notes:
+            note.pitch += numHalfSteps
+    return song
+
+# This method will change the key of an entire song from an origin key to a destination key
+# (assuming there are no key changes)
+#
+# <jfwiddif>
+def changeSongKey(song, originKey, destinationKey):
+    origin_index, destination_index, offset = None
+
+    # Get the index of the origin key
+    if originKey in NOTES:
+        origin_index = NOTES.index(originKey)
+    elif originKey in EQUIVALENCE:
+        origin_index = EQUIVALENCE.index(originKey)
+    else:
+        raise SyntaxError("Origin Key needs to be the key and #/b if necessary. Examples: 'C#', 'Db', 'F' etc")
+
+    # Get the index of the destination key
+    if destinationKey in NOTES:
+        destination_index = NOTES.index(destinationKey)
+    elif destinationKey in EQUIVALENCE:
+        destination_index = EQUIVALENCE.index(destinationKey)
+    else:
+        raise SyntaxError("Destination Key needs to be the key and #/b if necessary. Examples: 'C#', 'Db', 'F' etc")
+
+    # discover offset (this is the number of half steps to move each note to get to the destination key)
+    offset = destination_index - origin_index
+
+    #apply the offset to each note
+    for track in song.tracks:
+        for note in track.notes:
+            note.pitch += offset
+
+    return song
--- src/Note.py
+++ src/Note.py
@@ -5,7 +5,7 @@ class Note:
     # The absolute time this note occurs in the song
     time = 0
     # The duration of this note
-    duration = 0;
+    duration = 0
     # The intensity of the note
     velocity = 0
 
