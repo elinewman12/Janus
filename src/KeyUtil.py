cccd7c4f8b727f37a2268f1a4b6aa06be8b00ee4
John Widdifield <jfwiddif@ncsu.edu>
2021-09-08 15:21:33 -0400

    fixed KeyUtil Exception and manually tested

--- src/KeyUtil.py
+++ src/KeyUtil.py
@@ -18,7 +18,9 @@ def changeSongKeyByHalfSteps(song, numHalfSteps):
 #
 # <jfwiddif>
 def changeSongKey(song, originKey, destinationKey):
-    origin_index, destination_index, offset = None
+    origin_index = 0
+    destination_index = 0
+    offset = 0
 
     # Get the index of the origin key
     if originKey in NOTES:
@@ -44,4 +46,40 @@ def changeSongKey(song, originKey, destinationKey):
         for note in track.notes:
             note.pitch += offset
 
+    return song
+
+# This method will change the key of a section of a song from an origin key to a destination key
+# between the provided time intervals.  Time intervals are given in absolute
+#
+#
+# <jfwiddif>
+def changeKeyForInterval(song, originKey, destinationKey, intervalBegin, intervalEnd):
+    origin_index = 0
+    destination_index = 0
+    offset = 0
+
+    # Get the index of the origin key
+    if originKey in NOTES:
+        origin_index = NOTES.index(originKey)
+    elif originKey in EQUIVALENCE:
+        origin_index = EQUIVALENCE.index(originKey)
+    else:
+        raise SyntaxError("Origin Key needs to be the key and #/b if necessary. Examples: 'C#', 'Db', 'F' etc")
+
+    # Get the index of the destination key
+    if destinationKey in NOTES:
+        destination_index = NOTES.index(destinationKey)
+    elif destinationKey in EQUIVALENCE:
+        destination_index = EQUIVALENCE.index(destinationKey)
+    else:
+        raise SyntaxError("Destination Key needs to be the key and #/b if necessary. Examples: 'C#', 'Db', 'F' etc")
+
+    # discover offset (this is the number of half steps to move each note to get to the destination key)
+    offset = destination_index - origin_index
+
+    # apply the offset to each note within the time interval
+    for track in song.tracks:
+        for note in track.notes:
+            if intervalBegin <= note.time <= intervalEnd:
+                note.pitch += offset
     return song
