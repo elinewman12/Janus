206afbe02665e07b84a7e40f0eea33d92ae47471
Erin Litty <erlitty@ncsu.edu>
2021-09-28 12:39:15 -0400

    Create .coveragerc file, which will exclude unnecessary files from the coverage report

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 coverage: # Run tests with coverage
 	coverage erase
 	coverage run -m pytest -ra
-	coverage report -m --omit='/usr/local/lib/python3.9/*'
+	coverage report -m
 
 deps: # Install dependecies
 	pip3 install requirements.txt
--- test/test_song.py
+++ test/test_song.py
@@ -5,6 +5,7 @@ from control import Control
 from note import Note
 from song import Song
 from track import Track
+from key import Key
 
 import mido
 
@@ -57,7 +58,7 @@ def test_change_song_key():
     d_scale = Song()
 
     orig.load(filename="test/test MIDI/C_major_scale.mid")
-    orig = orig.change_song_key(origin_key='C', destination_key='D')
+    orig = orig.change_song_key(origin_key=Key('C'), destination_key=Key('D'))
     orig.save(filename="test/test MIDI/C_major_scale_Output.mid")
 
     new_song.load(filename="test/test MIDI/C_major_scale_Output.mid")
@@ -80,7 +81,7 @@ def test_change_key_for_interval():
     d_scale = Song()
 
     orig.load(filename="test/test MIDI/C_major_scale.mid")
-    orig = orig.change_key_for_interval(origin_key='C', destination_key='D', interval_begin=0, interval_end=107)
+    orig = orig.change_key_for_interval(origin_key=Key('C'), destination_key=Key('D'), interval_begin=0, interval_end=107)
     orig.save(filename="test/test MIDI/C_major_scale_Output.mid")
 
     new_song.load(filename="test/test MIDI/C_major_scale_Output.mid")
