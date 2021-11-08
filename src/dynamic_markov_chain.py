c1c9449e41a36a8be086040dc64ffdaaaa91e358
elinewman12 <elis.newman12@gmail.com>
2021-11-08 17:46:34 -0500

    Added docs
    Added documentation to the new code

--- src/dynamic_markov_chain.py
+++ src/dynamic_markov_chain.py
@@ -85,17 +85,30 @@ class DynamicMarkovChain:
         # Key is the same pattern, value is a list of all the percentages. [0, 0.5, 0.05, 0.25, ...]
 
     def generate_next_note(self, current_note_token):
+        """Given a token of previous notes, this will randomly generate a new note given the percetanges from
+            the markov chain object.
+
+            Args:
+                current_note_token (String): A string of 'token_length' notes in C-index
+
+            Returns:
+                int, String: An int of the next note and a string of the new pattern token.
+        """
         orig_token = current_note_token
+        # If this token is the end of the song, we need to begin in a new position
         if self.probabilities.get(current_note_token) is None:
+            # Let's check if there are any other similar patterns in the song
             for i in range(NUM_NOTES):
                 new_pattern = current_note_token[:len(current_note_token) - 2] + str(i)
                 if new_pattern in list(self.probabilities.keys()):
                     current_note_token = new_pattern
                     break
+            # If not, let's just randomly pick a new position to continue in the chain
             if orig_token == current_note_token:
                 current_note_token = py.random.choice(list(self.probabilities.keys()))
         note_follow = self.probabilities.get(current_note_token)
         percentage = []
+        # Create a 1-D array of the percentage of a given note with this pattern token
         for i in range(NUM_NOTES):
             found = False
             for note in note_follow:
@@ -104,23 +117,39 @@ class DynamicMarkovChain:
                     found = True
             if not found:
                 percentage.append(0)
+        # Pick a random new note with the percentages and return the note and new pattern
         next_note = py.random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1, p=percentage)
         return next_note[0], current_note_token
         # if self.type == Type.NOTE_LENGTH:
         #     raise NotImplementedError
 
     def generate_pattern(self, song, num_notes):
+        """Given a new song object and the number of notes to generate, this method will load that
+            amount of new notes into the song using the existing markov chain.
+
+            Args:
+                song (Song): A brand new Song object
+                num_notes (int): The number of notes to generate
+
+            Returns:
+                Song: The edited song object
+        """
+        # We are currently making every note an eighth note for easier testing
         eighth_note = int(song.ticks_per_beat / 2)
         t = Track()
         song.add_track(t)
+        # Start at a random place in the markov chain
         current_token = py.random.choice(list(self.probabilities.keys()))
         current_token_array = current_token.split()
+        # Add the beginning notes
         for i in range(self.token_length):
             t.add_note(Note(pitch=int(current_token_array[i]) + 35, time=0, duration=eighth_note))
-
+        # Iterate through the rest of the song
         for i in range(self.token_length, num_notes):
+            # Generate new note for song
             next_note_rtn = self.generate_next_note(current_token)
             next_note_tone, current_token = next_note_rtn[0], next_note_rtn[1]
+            # Create the new pattern with the new note
             previous_pattern = current_token.split()
             current_token = previous_pattern[1]
             for j in range(2, self.token_length):
