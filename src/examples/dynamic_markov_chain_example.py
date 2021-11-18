0800f87f45837082b709580aa5541d6434a84c9a
elinewman12 <esnewman@ncsu.edu>
2021-11-18 13:06:52 -0500

    Updated example
    Updated example for dynamic markov chain

--- src/examples/dynamic_markov_chain_example.py
+++ src/examples/dynamic_markov_chain_example.py
@@ -7,20 +7,23 @@ song.load(filename="../../MIDI Files/Rock/Greenday/GoodRiddance(TimeOfYourLife).
 chord_chain = DynamicMarkovChain("chord chain", token_length=3, chain_type=chainType.CHORD)
 note_chain = DynamicMarkovChain("note chain", token_length=3, chain_type=chainType.NOTE)
 
+chord_chain.add_song(song)
+note_chain.add_song(song)
+
 song2 = Song()
-chord_track1 = chord_chain.generate_pattern(song2, 8, 46)
-melody_track1 = note_chain.generate_pattern(song2, 32, 25)
+chord_track_verse = chord_chain.generate_pattern(song2, 8, 46)
+melody_track_verse = note_chain.generate_pattern(song2, 32, 25)
 
-chord_track2 = chord_chain.generate_pattern(song2, 8, 46)
-melody_track2 = note_chain.generate_pattern(song2, 32, 25)
+chord_track_chorus = chord_chain.generate_pattern(song2, 8, 46)
+melody_track_chorus = note_chain.generate_pattern(song2, 32, 25)
 
-chord_track = chord_track1.append_track(chord_track1)
-melody_track = melody_track1.append_track(melody_track1)
+chord_track = chord_track_verse.append_track(chord_track_verse)
+melody_track = melody_track_verse.append_track(melody_track_verse)
 
-chord_track = chord_track.append_track(chord_track2)
-chord_track = chord_track.append_track(chord_track2)
-melody_track = melody_track.append_track(melody_track2)
-melody_track = melody_track.append_track(melody_track2)
+chord_track = chord_track.append_track(chord_track_chorus)
+chord_track = chord_track.append_track(chord_track_chorus)
+melody_track = melody_track.append_track(melody_track_chorus)
+melody_track = melody_track.append_track(melody_track_chorus)
 
 song2.add_track(chord_track)
 song2.add_track(melody_track)
